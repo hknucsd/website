16a9250d380225b908dd25e71344c57070964212
jyeh2 <54336584+jyeh2@users.noreply.github.com>
2025-10-22 15:35:18 -0700

    Revert "Landing"

--- hkn-website/src/components/NavBar.jsx
+++ hkn-website/src/components/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import whiteHknLogo from '../media/logos/hkn-logo-white.png';
+import blackHknLogo from '../media/logos/hkn-logo-black.svg';
 
 function NavBar() {
   const location = useLocation();
   const isCareerFairPage = location.pathname === '/career-fair';
-  const logoSrc = isCareerFairPage ? whiteHknLogo : whiteHknLogo;
-  const textColorClass = isCareerFairPage ? 'text-white' : 'text-white';
+  const logoSrc = isCareerFairPage ? whiteHknLogo : blackHknLogo;
+  const textColorClass = isCareerFairPage ? 'text-white' : 'text-black';
   const hoverColorClass = isCareerFairPage ? 'hover:text-[#A3D7E5]' : 'hover:text-[#5F69A6]';
   
   return (
-    <nav className={`bg-transparent ${isCareerFairPage}`}>
+    <nav className={`bg-transparent ${isCareerFairPage ? '' : 'shadow-md bg-white bg-opacity-90'}`}>
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
